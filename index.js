7cb5316606408b6f4835bd7d497c2a3b2d3fe750
Etudiant O'clock <florian.le-galoudec@oclock.school>
2023-12-13 22:04:47 +0100

    bounce paddle ok

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ let startPositionOfLeftPaddleY = (canvas.height - paddleHeight) / 2;
 // raqette gauche
 let startPositionOfLeftPaddleX = canvas.width / 6 - paddleHeight;
 // raqette droite
-let startPositionOfRightPaddleX = canvas.width - startPositionOfLeftPaddleX- paddleWidth;
+let startPositionOfRightPaddleX =
+  canvas.width - startPositionOfLeftPaddleX - paddleWidth;
 //info de la balle
 const ballRadius = 10;
 const ballColor = "	#D742F5";
@@ -23,26 +24,33 @@ let y = canvas.height / 2;
 //mouvement aléatoire de la balle
 function randomStartLuncher() {
   const symboles = "+-";
-  const numbers = "23456";
+  const numbers = "234";
   randomDirection = symboles[Math.floor(Math.random() * symboles.length)];
-  randomNumber = parseInt(numbers[Math.floor(Math.random() * 5)]);
+  randomNumber = parseInt(numbers[Math.floor(Math.random() * 3)]);
   return randomDirection === "+" ? randomNumber : -randomNumber;
-  }
+}
+// fonction pour le départ de la balle
+// function startGameBall() {
+// //direction du mouvement de la balle
+// let dx = randomStartLuncher();
+// let dy = randomStartLuncher();
+
+// }
 //direction du mouvement de la balle
 let dx = randomStartLuncher();
 let dy = randomStartLuncher();
-// let dx = +2;
-// let dy = +2;
+// let dx = 2;
+// let dy = -2;
 let rightUpPressed = false;
 let rightDownPressed = false;
 let leftUpPressed = false;
 let leftDownPressed = false;
 // gestionnaires d'événements pour le déplacement de la raquette de droite
 document.addEventListener("keydown", keyDownHandlerRightPaddle, false);
-document.addEventListener("keyup", keyUpHandlerRightPaddle , false);
+document.addEventListener("keyup", keyUpHandlerRightPaddle, false);
 // gestionnaires d'événements pour le déplacement de la raquette de droite
 document.addEventListener("keydown", keyDownHandlerLeftPaddle, false);
-document.addEventListener("keyup", keyUpHandlerLeftPaddle , false);
+document.addEventListener("keyup", keyUpHandlerLeftPaddle, false);
 
 function drawLeftPaddle() {
   ctx.beginPath();
@@ -79,76 +87,123 @@ function drawBall() {
 }
 //fonctions relative au déplacement de la raquette droite
 function keyDownHandlerRightPaddle(e) {
-    if (e.key == "ArrowUp") {
-     rightUpPressed = true;
-    } else if (e.key == "ArrowDown" ) {
-      rightDownPressed = true;
-    }// Empêcher le défilement de la page lors de l'utilisation des touches fléchées
-    if (e.key === "ArrowUp" || e.key === "ArrowDown") {
-        e.preventDefault(); 
+  if (e.key == "ArrowUp") {
+    rightUpPressed = true;
+  } else if (e.key == "ArrowDown") {
+    rightDownPressed = true;
+  } // Empêcher le défilement de la page lors de l'utilisation des touches fléchées
+  if (e.key === "ArrowUp" || e.key === "ArrowDown") {
+    e.preventDefault();
+  }
+}
+
+function keyUpHandlerRightPaddle(e) {
+  if (e.key == "ArrowUp") {
+    rightUpPressed = false;
+  } else if (e.key == "ArrowDown") {
+    rightDownPressed = false;
+  }
+}
+
+function shifRightPaddle() {
+  if (rightDownPressed) {
+    startPositionOfRightPaddleY += 7;
+    if (startPositionOfRightPaddleY + paddleHeight > canvas.height) {
+      startPositionOfRightPaddleY = canvas.height - paddleHeight;
+    }
+  } else if (rightUpPressed) {
+    startPositionOfRightPaddleY -= 7;
+    if (startPositionOfRightPaddleY < 0) {
+      startPositionOfRightPaddleY = 0;
     }
   }
-  
-  function keyUpHandlerRightPaddle(e) {
-    if (e.key == "ArrowUp") {
-      rightUpPressed = false;
-    } else if (e.key == "ArrowDown" ) {
-     rightDownPressed = false;
-    }
-  }
-  
-  function shifRightPaddle() {
-    if (rightDownPressed) {
-        startPositionOfRightPaddleY += 7;
-        if (startPositionOfRightPaddleY + paddleHeight > canvas.height) {
-            startPositionOfRightPaddleY = canvas.height - paddleHeight;
-            
-        }
-      }  else if (rightUpPressed)  {
-       startPositionOfRightPaddleY -= 7;
-       if (startPositionOfRightPaddleY < 0) {
-         startPositionOfRightPaddleY = 0 ;
-       }
-      }
-  }
+}
 //fonctions relative au déplacement de la raquette droite
-  function keyDownHandlerLeftPaddle(e) {
-    if (e.key == "q") {
-     leftUpPressed = true;
-    } else if (e.key == "w" ) {
-      leftDownPressed = true;
-    }// Empêcher le défilement de la page lors de l'utilisation des touches fléchées
-    if (e.key === "q" || e.key === "w") {
-        e.preventDefault(); 
-    }
+function keyDownHandlerLeftPaddle(e) {
+  if (e.key == "q") {
+    leftUpPressed = true;
+  } else if (e.key == "w") {
+    leftDownPressed = true;
+  } // Empêcher le défilement de la page lors de l'utilisation des touches fléchées
+  if (e.key === "q" || e.key === "w") {
+    e.preventDefault();
   }
-  
-  function keyUpHandlerLeftPaddle(e) {
-    if (e.key == "q") {
-      leftUpPressed = false;
-    } else if (e.key == "w" ) {
-     leftDownPressed = false;
-    }
+}
+
+function keyUpHandlerLeftPaddle(e) {
+  if (e.key == "q") {
+    leftUpPressed = false;
+  } else if (e.key == "w") {
+    leftDownPressed = false;
   }
+}
 
 function shifLeftPaddle() {
-    if (leftDownPressed) {
-        startPositionOfLeftPaddleY += 7;
-        if (startPositionOfLeftPaddleY + paddleHeight > canvas.height) {
-            startPositionOfLeftPaddleY = canvas.height - paddleHeight;
-            
+  if (leftDownPressed) {
+    startPositionOfLeftPaddleY += 7;
+    if (startPositionOfLeftPaddleY + paddleHeight > canvas.height) {
+      startPositionOfLeftPaddleY = canvas.height - paddleHeight;
+    }
+  } else if (leftUpPressed) {
+    startPositionOfLeftPaddleY -= 7;
+    if (startPositionOfLeftPaddleY < 0) {
+      startPositionOfLeftPaddleY = 0;
+    }
+  }
+}
+
+function paddleCollision() {
+      // Collision avec la raquette gauche
+      if (
+        x - ballRadius < startPositionOfLeftPaddleX + paddleWidth &&
+        x + ballRadius > startPositionOfLeftPaddleX &&
+        y > startPositionOfLeftPaddleY &&
+        y < startPositionOfLeftPaddleY + paddleHeight
+      ) {
+        dx = -dx; // Inverser la direction horizontale
+        if (
+          y - ballRadius < startPositionOfLeftPaddleY + paddleHeight &&
+          y + ballRadius > startPositionOfLeftPaddleY &&
+          x > startPositionOfLeftPaddleX &&
+          x < startPositionOfLeftPaddleX + paddleWidth
+        ) {
+          dy = -dy; // Inverser la direction verticale
+          
         }
-      }  else if (leftUpPressed)  {
-       startPositionOfLeftPaddleY -= 7;
-       if (startPositionOfLeftPaddleY < 0) {
-         startPositionOfLeftPaddleY = 0 ;
-       }
       }
-  }
+    
+      // Collision avec la raquette droite
+      if (
+        x + ballRadius > startPositionOfRightPaddleX &&
+        x - ballRadius < startPositionOfRightPaddleX + paddleWidth &&
+        y > startPositionOfRightPaddleY &&
+        y < startPositionOfRightPaddleY + paddleHeight
+      ) {
+        dx = -dx; // Inverser la direction horizontale
+        if (
+          y + ballRadius < startPositionOfLeftPaddleY + paddleHeight &&
+          y - ballRadius > startPositionOfLeftPaddleY &&
+          x > startPositionOfLeftPaddleX &&
+          x < startPositionOfLeftPaddleX + paddleWidth
+        ) {
+          dy = -dy; // Inverser la direction verticale
+          
+        }
+      }
+}
 
-  function collisionDetection() {
 
-  }
+
+function bounceWall() {
+ //rebond de la balle sur les murs inferieur || supérieur
+ if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+  dy = -dy;
+}
+//rebond de la balle sur les murs droite || gauche
+if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+ dx = -dx;
+}
+}
 
 //peindre le jeu
 function draw() {
@@ -157,24 +212,13 @@ function draw() {
   drawLeftPaddle();
   drawRightPaddle();
   drawBall();
-  collisionDetection();
- 
-  //rebond de la balle sur le mur inferieur || supérieur
-  if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
-    dy = -dy;
-  } 
-  // il y a trop de if j'ai l'impression de tuer des p'tits chats 
-//rebond de la balle sur les côtés gauche et droit
-if (x + dx > canvas.width - ballRadius || x + dx < 0 + ballRadius) {
-    if (y > startPositionOfRightPaddleY && y < startPositionOfRightPaddleY + paddleHeight) {
-        dy = -dy; // Rebond sur la raquette droite
-    } else if (y > startPositionOfLeftPaddleY && y < startPositionOfLeftPaddleY + paddleHeight) {
-        dy = -dy; // Rebond sur la raquette gauche
-    }
-}
+  bounceWall()
+  paddleCollision();
+  
+  
 
- shifRightPaddle()
-  shifLeftPaddle()
+  shifRightPaddle();
+  shifLeftPaddle();
   x += dx;
   y += dy;
 }
